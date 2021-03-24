ad56ed32053507ef5c94feb97a07b84c83358a8b
Chanson9892 <chan.hanson5@gmail.com>
2021-03-24 16:38:28 -0700

    shows apod info on frontend

--- app/controllers/nasa_api_controller.rb
+++ app/controllers/nasa_api_controller.rb
@@ -2,7 +2,7 @@ class NasaApiController < ApplicationController
     NASA_API_KEY = ENV['nasa_api_key']
     
     def show
-        response = RestClient.get "https://api.nasa.gov/planetary/apod?date=#{params[:date]}&hd=true&api_key=#{NASA_API_KEY}"
+        response = RestClient.get "https://api.nasa.gov/planetary/apod?date=#{params[:date]}&hd=true&thumbs=true&api_key=#{NASA_API_KEY}"
         result = JSON.parse(response)
         render json: result
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resourcs :nasa_api
+  resources :nasa_api
+  get '/get_image', to: 'nasa_api#show'
 end
